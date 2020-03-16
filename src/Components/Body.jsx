0b8901ad593e0bbc03ea3d38eaf9b3b2b6a99d9a
Alberto Vega <albertovega00@gmail.com>
2020-03-16 16:00:51 -0700

    sending data from server to the front end

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -1,8 +1,24 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 export default class Body extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            pokemon: []
+        }
+    }
 
-    render() {
+    componentDidMount() {
+        axios.get(`/game/:pokemon`)
+            .then(res => {
+                const pokemon = res.data;
+                this.setState({ pokemon: pokemon });
+            })
+        }
+        
+        render() {
+            console.log(this.state.pokemon)
         return (
             <div>
                 Hello world
