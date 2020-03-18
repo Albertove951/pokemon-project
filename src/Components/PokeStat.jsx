2c73d6be9f8d14f6257d45e8c7f06f8944cc6777
Alberto Vega <albertovega00@gmail.com>
2020-03-18 15:42:19 -0700

    added content to card

--- src/Components/PokeStat.jsx
+++ src/Components/PokeStat.jsx
@@ -6,8 +6,10 @@ export default class PokeStat extends Component {
         return(
             <div className="card poke-container">
                 <p>Pikachu</p>
-                <img src="" alt=""/>
-                <p></p>
+                
+                <img src="https://tr.rbxcdn.com/19d61db0b07263298eabf0ebb86fb6de/420/420/Decal/Png" alt="" height="200px" width="200px" className="poke-img"/>
+                
+                <p>Whenever Pikachu comes across something new, it blasts it with a jolt of electricity. If you come across a blackened berry, it's evidence that this Pokémon mistook the intensity of its charge.</p>
             </div>
         )
     }
